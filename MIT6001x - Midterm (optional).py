2910dc73fc18cd3d6758a41679da2cfe95aa5fa8
Yulia Zamriy <yulia.zamriy@gmail.com>
2017-07-09 22:12:21 -0400

    added two more problems

--- MIT6001x - Midterm (optional).py
+++ MIT6001x - Midterm (optional).py
@@ -9,6 +9,7 @@ def closest_power(base, num):
     In case of a tie, return the smaller value.
     Returns the exponent.
     '''
+    
     e1 = int(log(num,base))
     e2 = e1 + 1
     return e1 if abs(base**e1 - num) <= abs(base**e2 - num) else e2
@@ -19,5 +20,39 @@ def closest_power(base, num):
 #   for i in (e-1,e, e+1):
 #       min_e[i] = (abs(base**i - num), base**i - num)
 #   return min(min_e, key=min_e.get)
-closest_power(82, 1)
-#print(closest_power(20, 210.0))
+
+#closest_power(82, 1)
+print(closest_power(20, 210.0))
+
+#%%
+
+def dotProduct(listA, listB):
+    '''
+    listA: a list of numbers
+    listB: a list of numbers of the same length as listA
+    '''
+    product = 0
+    for i, j in zip(listA, listB):
+        product += i*j
+    return product
+
+listA = [1, 2, 3] 
+listB = [4, 5, 6]
+dotProduct(listA, listB)
+
+#%%
+
+def deep_reverse(L):
+    """ assumes L is a list of lists whose elements are ints
+    Mutates L such that it reverses its elements and also 
+    reverses the order of the int elements in every element of L. 
+    It does not return anything.
+    """
+    for i in L[::-1]:
+        L.remove(i)
+        L.append(i[::-1])
+    
+#L = [[1, 2], [3, 4], [5, 6, 7]]
+L = [[0, 1, 2], [1, 2, 3], [3, 2, 1], [10, -10, 100]]
+deep_reverse(L) 
+print(L)
