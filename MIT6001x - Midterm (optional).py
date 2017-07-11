f35866a5e1cf14fa885e51cb1273b60c41447fe5
Yulia Zamriy <yulia.zamriy@gmail.com>
2017-07-10 23:19:29 -0400

    added all the remaining problems

--- MIT6001x - Midterm (optional).py
+++ MIT6001x - Midterm (optional).py
@@ -56,3 +56,82 @@ def deep_reverse(L):
 L = [[0, 1, 2], [1, 2, 3], [3, 2, 1], [10, -10, 100]]
 deep_reverse(L) 
 print(L)
+
+#%%
+
+def f(a, b):
+    return a + b
+
+def dict_interdiff(d1, d2):
+    '''
+    d1, d2: dicts whose keys and values are integers
+    Returns a tuple of dictionaries according to the instructions above
+    '''
+    intersect = {}
+    difference = {}
+    
+    intersect = {i:f(d1[i],d2[i]) for i in d1 if i in d2}
+    
+    for i in d1.keys() ^ d2.keys():
+        difference[i] = d1.get(i, d2.get(i))
+
+    return (intersect, difference)
+
+d1 = {1:30, 2:20, 3:30, 5:80}
+d2 = {1:40, 2:50, 3:60, 4:70, 6:90}
+dict_interdiff(d1, d2)
+
+#%%
+
+def f(i):
+    return i + 2
+def g(i):
+    return i > 5
+
+def applyF_filterG(L, f, g):
+    """
+    Assumes L is a list of integers
+    Assume functions f and g are defined for you. 
+    f takes in an integer, applies a function, returns another integer 
+    g takes in an integer, applies a Boolean function, 
+        returns either True or False
+    Mutates L such that, for each element i originally in L, L contains  
+        i if g(f(i)) returns True, and no other elements
+    Returns the largest element in the mutated L or -1 if the list is empty
+    """
+    
+    for i in L[:]:
+        if g(f(i)) is False:
+            L.remove(i)
+    return -1 if not L else max(L)
+
+# L = [0, -10, 5, 6, -4]
+# L = [0, -10, -5, -6, -4]
+# L = []
+# L = [0, -10, 5, 5, -4]
+L = [6, 5, 5]
+print(applyF_filterG(L, f, g))
+print(L)
+
+#%%
+
+def flatten(aList):
+    ''' 
+    aList: a list 
+    Returns a copy of aList, which is a flattened version of aList 
+    '''
+    aListf = []
+    for i in aList:
+        if not isinstance(i, list):
+            aListf.append(i)
+        else:
+            for j in flatten(i):
+                aListf.append(j)
+    
+    return(aListf)
+    
+aList = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+# aList = [[1,2,3],[4,5,6], [7], [8,9]]
+# aList = [[1],2,3]
+flatten(aList)
+
